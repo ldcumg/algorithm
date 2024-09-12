919a59cb896b87564a9576bf137bec6d19d05225
ldcumg <ldcumg@naver.com>
2024-09-13 00:07:39 +0900

    [level 1] Title: 하샤드 수, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
@@ -1,4 +1,4 @@
 function solution(x) {
     const arrayX = Array.from(String(x),strX=>Number(strX))
-    return x % arrayX.reduce((acc,cur)=>acc+cur) ? false : true
+    return !(x % arrayX.reduce((acc,cur)=>acc+cur))
 }
